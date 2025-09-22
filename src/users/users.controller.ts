32d349f111018075dc23a88aa7da0182c6cec94f
jungyh <jungyh870918@gmail.com>
2025-09-22 16:27:05 +0900

    변경전 임시저장

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,6 +20,8 @@ import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
 import { AuthGuard } from '../guards/auth.guard';
+import * as jwt from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -27,6 +29,7 @@ export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService,
+    private configService: ConfigService,
   ) {}
 
   @Get('/whoami')
@@ -43,6 +46,17 @@ export class UsersController {
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signup(body.email, body.password);
+    console.log(user);
+    console.log(user);
+    console.log(user);
+    console.log(user);
+    console.log(user);
+    console.log(user);
+    const token = jwt.sign(
+      { sub: user.id },
+      this.configService.get('JWT_SECRET'),
+      { expiresIn: this.configService.get('JWT_EXPIRES_IN') || '1d' },
+    );
     session.userId = user.id;
     return user;
   }
@@ -51,6 +65,11 @@ export class UsersController {
   async signin(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signin(body.email, body.password);
     session.userId = user.id;
+    const token = jwt.sign(
+      { sub: user.id },
+      this.configService.get('JWT_SECRET'),
+      { expiresIn: this.configService.get('JWT_EXPIRES_IN') || '1d' },
+    );
     return user;
   }
 
